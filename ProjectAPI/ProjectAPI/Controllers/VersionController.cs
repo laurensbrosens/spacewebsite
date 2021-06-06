51b545189fd3bf2ee5e8d64ff4167d3b9603dbef
laurens <laurens.brosens@student.ap.be>
2021-06-06 19:20:35 +0200

    filmpje

--- ProjectAPI/ProjectAPI/Controllers/VersionController.cs
+++ ProjectAPI/ProjectAPI/Controllers/VersionController.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 
 namespace ProjectAPI.Controllers
-{
+{//
     [Route("api/v1")]
     [ApiController]
     public class VersionController : ControllerBase
